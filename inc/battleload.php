5f1355f84419226b6768a765d9046bbaa6b6ddb3
Tom Johnson <ATomIK@users.noreply.github.com>
2016-10-31 14:02:55 -0700

    Update battleload.php
    Added a steam id 64 to 32 conversion

--- inc/battleload.php
+++ inc/battleload.php
@@ -2,13 +2,16 @@
 
 include('config.php');
 
-$steamid = $_GET["steamid"];
-if(!isset($_GET["steamid"])){
-  $steamid = "76561198074425791" ;
-};
+if(!isset($_GET['steamid']){
+  $s64 = '76561198074425791';
+} else {
+  $s64 = $_GET['steamid'];
+}
+   
+$type = (substr($s64, -1) & 1) ? 1 : 0;
+$steamid = sprintf("STEAM_0:%d:%d", $type, (bcsub($s64, "76561197960265728") - $type) / 2);
 
-
-$url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".$SteamAPIKey."&steamids=".$steamid;
+$url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".$SteamAPIKey."&steamids=".$s64;
 $json = file_get_contents($url);
 $getdata = json_decode($json, true);
 $data = $getdata["response"]["players"][0];
